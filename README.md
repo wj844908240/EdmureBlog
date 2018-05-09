0feda61127e7a66ae26a74256c9f95cdd2c379cc
wangji123 <2210343034@qq.com>
2018-05-09 12:27:47 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# EdmureBlog
+
+
+# 使用django开发博客项目
---
+++ web/views/home.py
@@ -0,0 +1,155 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+from django.shortcuts import render
+from django.shortcuts import redirect
+from repository import models
+from utils.pagination import Pagination
+from django.urls import reverse
+
+def index(request, *args, **kwargs):
+    """
+    博客首页，展示全部博文
+    :param request:
+    :return:
+    """
+
+    article_type_list = models.Article.type_choices
+
+    if kwargs:
+
+        article_type_id = int(kwargs['article_type_id'])
+        #url  name的别称 在html与view的用法不同  如果没有定义名称 这里传入的是 args=args
+        base_url = reverse('index',kwargs=kwargs)
+    else:
+        article_type_id = None
+        base_url = '/'
+
+    data_count = article_list = models.Article.objects.filter(**kwargs).count()
+
+
+    page_obj = Pagination(request.GET.get('p'),data_count)
+    article_list = models.Article.objects.filter(**kwargs).order_by('-nid')[page_obj.start:page_obj.end]
+    #base_url 获取到页面的url用于分页
+    page_str = page_obj.page_str(base_url)
+
+    return render(
+        request,
+        'index.html',
+        {
+            'article_list': article_list,
+            'article_type_id': article_type_id,
+            'article_type_list': article_type_list,
+            'page_str': page_str,
+        }
+    )
+
+
+def home(request, site):
+    """
+    博主个人首页
+    :param request:
+    :param site: 博主的网站后缀如：http://xxx.com/wupeiqi.html
+    :return:
+    """
+    #select_related  连表查询优化性能  一次查询
+    blog = models.Blog.objects.filter(site=site).select_related('user').first()
+    if not blog:
+        return redirect('/')
+    tag_list = models.Tag.objects.filter(blog=blog)
+    category_list = models.Category.objects.filter(blog=blog)
+    date_list = models.Article.objects.raw(
+        'select nid, count(nid) as num,strftime("%Y-%m",create_time) as ctime from repository_article group by strftime("%Y-%m",create_time)')
+
+    article_list = models.Article.objects.filter(blog=blog).order_by('-nid').all()
+
+    return render(
+        request,
+        'home.html',
+        {
+            'blog': blog,
+            'tag_list': tag_list,
+            'category_list': category_list,
+            'date_list': date_list,
+            'article_list': article_list
+        }
+    )
+
+
+def filter(request, site, condition, val):
+    """
+    分类显示
+    :param request:
+    :param site:
+    :param condition:
+    :param val:
+    :return:
+    """
+
+    #获取当前的博客
+    blog = models.Blog.objects.filter(site=site).select_related('user').first()
+    if not blog:
+        return redirect('/')
+    tag_list = models.Tag.objects.filter(blog=blog)
+    category_list = models.Category.objects.filter(blog=blog)
+    date_list = models.Article.objects.raw(
+        'select nid, count(nid) as num,strftime("%Y-%m",create_time) as ctime from repository_article group by strftime("%Y-%m",create_time)')
+
+    template_name = "home_summary_list.html"
+    if condition == 'tag':
+        template_name = "home_title_list.html"
+        article_list = models.Article.objects.filter(tags=val, blog=blog).all()
+    elif condition == 'category':
+        article_list = models.Article.objects.filter(category_id=val, blog=blog).all()
+    elif condition == 'date':
+        # article_list = models.Article.objects.filter(blog=blog).extra(
+        # where=['date_format(create_time,"%%Y-%%m")=%s'], params=[val, ]).all()
+        article_list = models.Article.objects.filter(blog=blog).extra(
+            #%%的目的是输出%  目的是在数据库输出%Y-%m
+            #strftime  是django 内置的  mysql应该用'date_format(create_time,"%%Y-%%m")=%s'
+            where=['strftime("%%Y-%%m",create_time)=%s'], params=[val, ]).all()
+    else:
+        article_list = []
+
+    return render(
+        request,
+        template_name,
+        {
+            'blog': blog,
+            'tag_list': tag_list,
+            'category_list': category_list,
+            'date_list': date_list,
+            'article_list': article_list
+        }
+    )
+
+
+def detail(request, site, nid):
+    """
+    博文详细页
+    :param request:
+    :param site:
+    :param nid:
+    :return:
+    """
+    blog = models.Blog.objects.filter(site=site).select_related('user').first()
+    tag_list = models.Tag.objects.filter(blog=blog)
+    category_list = models.Category.objects.filter(blog=blog)
+    date_list = models.Article.objects.raw(
+        'select nid, count(nid) as num,strftime("%Y-%m",create_time) as ctime from repository_article group by strftime("%Y-%m",create_time)')
+
+    article = models.Article.objects.filter(blog=blog, nid=nid).select_related('category', 'articledetail').first()
+    comment_list = models.Comment.objects.filter(article=article).select_related('reply')
+    return render(
+        request,
+        'home_detail.html',
+        {
+            'blog': blog,
+            'article': article,
+            'comment_list': comment_list,
+            'tag_list': tag_list,
+            'category_list': category_list,
+            'date_list': date_list,
+        }
+
+    )
+
